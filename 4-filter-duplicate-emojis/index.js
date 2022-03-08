e00e6c6f3e7657fe55fe71933041c397af06ef3f
darling_d <ddarling212@gmail.com>
2022-03-08 18:28:38 -0500

    Coded Day 4

---
+++ 4-filter-duplicate-emojis/index.js
@@ -0,0 +1,26 @@
+document.getElementById("btn").addEventListener("click", filterDuplicates)
+
+const emojis = [
+    "💘","💘","🌞","🧡","🍇",
+    "🧁","⚡️","💝","🐱","💘",
+    "🧡","💫","🍓","💜","⚡️",
+    "⚡️","🐶","💘","🍇","🐱",
+    "💘","💫","🍓","💜","💕",
+    "⚡️","💘","🌞","🐱","💘",
+    "💫","🍓","💜","⚡️","🧡",
+    "💕","🍇","💘"
+    ]
+    
+const allEmojisP = document.getElementById("all-emojis-p")
+const uniqueEmojisP = document.getElementById("unique-emojis-p")
+
+for (let emoji of emojis) {
+    allEmojisP.textContent += emoji
+}
+
+function filterDuplicates() {
+    let unique = [...new Set(emojis)];
+    for (let emoji of unique) {
+        uniqueEmojisP.textContent += emoji
+    }
+}
